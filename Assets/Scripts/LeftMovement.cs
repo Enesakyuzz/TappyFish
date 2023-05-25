c812d5e1d05a1fce85cd57905d324b68c1c6a12a
Enes <enesak10030@gmail.com>
2023-05-25 15:47:01 +0300

    ObstacleSpawn
    Obstacle Spawn

--- Assets/Scripts/LeftMovement.cs
+++ Assets/Scripts/LeftMovement.cs
@@ -7,21 +7,41 @@ public class LeftMovement : MonoBehaviour
     public float speed;
     BoxCollider2D box;
     float groundWidth;
+    float obstacleWidht;
     void Start()
     {
-        box = GetComponent<BoxCollider2D>();
-        groundWidth = box.size.x;
+        if(gameObject.CompareTag("Ground"))
+        {
+            box = GetComponent<BoxCollider2D>();
+            groundWidth = box.size.x;
+        }
+        else if(gameObject.CompareTag("Obstacle"))
+        {
+            obstacleWidht = GameObject.FindGameObjectWithTag("Column").GetComponent<BoxCollider2D>().size.x;
+        }    
     }
-
-
+    
     void Update()
     {
+
         transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
 
-        if(transform.position.x <= -groundWidth)
-        {
-            transform.position = new Vector2(transform.position.x +2 * groundWidth, transform.position.y);
+        if (gameObject.CompareTag("Ground"))
+        { 
+            if(transform.position.x <= -groundWidth)
+            {
+                transform.position = new Vector2(transform.position.x +2 * groundWidth, transform.position.y);
+            }
         }
+
+        else if(gameObject.CompareTag("Obstacle"))
+        {
+            if (transform.position.x < GameManager.bottomLeft.x - obstacleWidht)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
     }
 
 }
---
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public GameObject obstacle;
+    public float maxTime;
+    float timer; 
+    public float maxY;
+    public float minY;
+    float randomY;
+
+  
+    void Start()
+    {
+        InstantiateObstacle();
+    }
+
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= maxTime)
+        {
+            randomY = Random.Range(minY,maxY);
+            InstantiateObstacle();
+            timer = 0;
+        }
+    }
+
+    public void InstantiateObstacle()
+    {
+        GameObject newobstacle = Instantiate(obstacle);
+        newobstacle.transform.position = new Vector2(transform.position.x, randomY);
+    }
+}
